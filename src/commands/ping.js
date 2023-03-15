b1ad5439ae502c87317caa691b343e54585e9129
garvsl <garvsehgal7@gmail.com>
2023-03-15 17:22:45 -0400

    ping command

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -4,11 +4,13 @@ const wait = require("node:timers/promises").setTimeout;
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("ping")
-    .setDescription("Replies with Pong!"),
+    .setDescription("Replies with server ping!"),
   async execute(interaction) {
-    await interaction.reply("Pong!");
-    await wait(2000);
-    await interaction.deleteReply();
-    await interaction.followUp("Pong again!");
+    await interaction.deferReply();
+    const reply = await interaction.fetchReply();
+    const ping = reply.createdTimestamp - interaction.createdTimestamp;
+    interaction.editReply(
+      `Pong! Client: ${ping}ms | Websocket: ${interaction.client.ws.ping}ms`
+    );
   },
 };
